efdb716049b1d2d37359577d422ac447428afed8
immadisairaj <immadirajendra.sai@gmail.com>
2018-07-23 23:36:14 +0530

    Quiz Added Tabs

--- Quiz/app/src/main/java/com/example/immad/quiz/SimpleFragmentPagerAdapter.java
+++ Quiz/app/src/main/java/com/example/immad/quiz/SimpleFragmentPagerAdapter.java
@@ -1,13 +1,18 @@
 package com.example.immad.quiz;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
 
-    SimpleFragmentPagerAdapter(FragmentManager fm) {
+    private Context mContext;
+
+    SimpleFragmentPagerAdapter(Context context, FragmentManager fm) {
         super(fm);
+        mContext = context;
     }
 
     @Override
@@ -16,6 +21,19 @@ class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
             return new AnswersFragment();
         } else {
             return new QuestionsFragment();
+        }
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch(position) {
+            case 0:
+                return mContext.getString(R.string.answers);
+            case 1:
+                return mContext.getString(R.string.q_reference);
+            default:
+                return null;
         }
     }
 
--- Quiz/app/src/main/java/com/example/immad/quiz/Solution.java
+++ Quiz/app/src/main/java/com/example/immad/quiz/Solution.java
@@ -2,6 +2,7 @@ package com.example.immad.quiz;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -17,6 +18,9 @@ public class Solution extends AppCompatActivity {
     @BindView(R.id.viewpager)
     ViewPager viewPager;
 
+    @BindView(R.id.sliding_tabs)
+    TabLayout tabLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +29,10 @@ public class Solution extends AppCompatActivity {
 
         Answers = getIntent().getIntegerArrayListExtra("Answer");
 
-        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(this, getSupportFragmentManager());
         viewPager.setAdapter(adapter);
+
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     public static ArrayList<Integer> getAnswer() {
